7fb065b7182549132b5db8adac8c2e8aa911bc25
pudost <pudostt@gmail.com>
2026-08-04 14:22:37 -0600

    tercer commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import hashlib 
+import sqlite3
+import pathlib
+import json
+import os
+import uuid
+import datetime
+import PySide6
+
+from PySide6.QtWidgets import QApplication, QWidget
+
+app = QApplication()
+widget = QWidget()
+widget.show()
+app.exec()
