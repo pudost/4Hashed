a1ff8169fe2bd148708e0ce57bfab1c0ff64cb76
pudost <pudostt@gmail.com>
2026-08-13 11:38:56 -0600

    ahora el gitignore esta y eh arreglado a .venv que no deberia haber estado para subirse a github

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import hashlib 
-import sqlite3
-import pathlib
-import json
-import os
-import uuid
-import datetime
-import PySide6
-
 from PySide6.QtWidgets import QApplication, QWidget
-
+from ui.pages.login_page import loginwindow
+window = loginwindow()
 app = QApplication()
 widget = QWidget()
 widget.show()
